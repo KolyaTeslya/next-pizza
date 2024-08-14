f65bfca60e70f93577ae11054e0ed8b48d3669d1
Kolya <kolya.teslya9421@gmail.com>
2024-08-14 20:30:03 +0300

    add prisma scheme and many changes

--- src/components/shared/categories.tsx
+++ src/components/shared/categories.tsx
@@ -1,28 +1,31 @@
 'use client';
 
 import { cn } from '@/lib/utils';
-import React from 'react';
+// import { useCategoryStore } from '@/shared/store/category';
 
+// import { Category } from '@prisma/client';
+import React from 'react';
+import { useCategoryStore } from '../../../store/category';
+ 
 interface Props {
+  items: Category[];
   className?: string;
 }
 
-export const Categories: React.FC<Props> = ({  className }) => {
-
-    const cats = ["Пиццы", "Комбо", "Закуски", "Коктейли", "Кофе", "Напитки"]
-    const activeIndex = 0;
-
+export const Categories: React.FC<Props> = ({ items, className }) => {
+  const categoryActiveId = useCategoryStore((state) => state.activeId);
 
   return (
     <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
-      {cats.map(( cat, index) => (
+      {items.map(({ name, id }, index) => (
         <a
           className={cn(
             'flex items-center font-bold h-11 rounded-2xl px-5',
-            activeIndex === index && 'bg-white shadow-md shadow-gray-200 text-primary',
+            categoryActiveId === id && 'bg-white shadow-md shadow-gray-200 text-primary',
           )}
+          href={`/#${name}`}
           key={index}>
-          <button>{cat}</button>
+          <button>{name}</button>
         </a>
       ))}
     </div>
