4693a158ec707167a96edc2ef79198ba68455416
Kolya <kolya.teslya9421@gmail.com>
2025-02-01 15:20:55 +0200

    add scroll categories

--- src/components/shared/categories.tsx
+++ src/components/shared/categories.tsx
@@ -5,34 +5,31 @@ import { cn } from '@/lib/utils';
 
 import { Category } from '@prisma/client';
 import React from 'react';
-// import { useCategoryStore } from '../../../store/category';
+import { useCategoryStore } from '../../../store/category';
  
 interface Props {
   items: Category[];
   className?: string;
 }
 
-export const Categories: React.FC<Props> = ({ items = [], className }) => {
-  // const categoryActiveId = useCategoryStore((state) => state.activeId);
+export const Categories: React.FC<Props> = ({ items, className }) => {
+  const categoryActiveId = useCategoryStore((state) => state.activeId);
 
   return (
     <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
-      {items.length > 0 ? (
-        items.map(({ name, id }, index) => (
-          <a
-            className={cn(
-              'flex items-center font-bold h-11 rounded-2xl px-5',
-              // categoryActiveId === id && 'bg-white shadow-md shadow-gray-200 text-primary',
-            )}
-            href={`/#${name}`}
-            key={index}>
-            <button>{name}</button>
-          </a>
-        ))
-      ) : (
-        <p>No categories available</p> // Можно отобразить сообщение, если `items` пустой
-      )}
+      {items.map(({ name, id }, index) => (
+        <a
+          className={cn(
+            'flex items-center font-bold h-11 rounded-2xl px-5',
+            categoryActiveId === id && 'bg-white shadow-md shadow-gray-200 text-primary',
+          )}
+          href={`/#${name}`}
+          key={index}>
+          <button>{name}</button>
+        </a>
+      ))}
     </div>
   );
 };
+
 
